a94ee3046069f670c774a3dabd37f6cc1c95cb65
Ajith Kumar M <ajith.km@stellaripl.com>
2024-06-05 15:53:22 +0000

    Update OrderFormController.php

--- app/Http/Controllers/OrderFormController.php
+++ app/Http/Controllers/OrderFormController.php
@@ -312,115 +312,19 @@ class OrderFormController extends Controller
         }
     }
 
-    public function coversheet_prep(Request $request, $orderId = null){
-        
+public function coversheet_prep(Request $request, $orderId = null){
         $user = Auth::user();
-        $processIds = $this->getProcessIdsBasedOnUserRole($user);
+        $orderData = OrderCreation::find($orderId);
 
-        $query = DB::table('oms_order_creations')
-            ->leftJoin('stl_item_description', 'oms_order_creations.process_id', '=', 'stl_item_description.id')
-            ->leftJoin('oms_state', 'oms_order_creations.state_id', '=', 'oms_state.id')
-            ->leftJoin('county', 'oms_order_creations.county_id', '=', 'county.id')
-            ->leftJoin('oms_status', 'oms_order_creations.status_id', '=', 'oms_status.id')
-            ->leftJoin('oms_users as assignee_users', 'oms_order_creations.assignee_user_id', '=', 'assignee_users.id')
-            ->leftJoin('oms_users as assignee_qas', 'oms_order_creations.assignee_qa_id', '=', 'assignee_qas.id')
-            ->select(
-                'oms_order_creations.id',
-                'oms_order_creations.order_id as order_id',
-                'oms_order_creations.status_id as status_id',
-                'oms_order_creations.county_id as county_id',
-                'oms_order_creations.process_id as process_id',
-                'oms_order_creations.order_date as order_date',
-                'stl_item_description.project_code as project_code',
-                'stl_item_description.process_name as process_name',
-                'stl_item_description.qc_enabled as qc_enabled',
-                'oms_state.short_code as short_code',
-                'county.county_name as county_name',
-                'oms_order_creations.assignee_user_id',
-                'oms_order_creations.assignee_qa_id',
-                DB::raw('CONCAT(assignee_users.emp_id, " (", assignee_users.username, ")") as assignee_user'),
-                DB::raw('CONCAT(assignee_qas.emp_id, " (", assignee_qas.username, ")") as assignee_qa')
-            )
-            ->where('oms_order_creations.is_active', 1)->where('oms_order_creations.id', $orderId);
-
-            $query->whereIn('oms_order_creations.process_id', $processIds);
-
-
-
-            if (
-                isset($request->status) &&
-                in_array($request->status, [1, 2, 3, 4, 5, 13, 14]) &&
-                $request->status != 'All' &&
-                $request->status != 6 &&
-                $request->status != 7
-            ) {
-                if ($request->status == 1) {
-                    if(in_array($user->user_type_id, [1, 2, 3, 4, 5, 9, 13, 14])) {
-                        $query->where('oms_order_creations.status_id', $request->status)->whereNotNull('oms_order_creations.assignee_user_id');
-                    } else {
-                        $query->where('oms_order_creations.status_id', $request->status)->where('oms_order_creations.assignee_user_id', $user->id);
-                    }
-                } elseif($request->status == 4) {
-                    if(in_array($user->user_type_id, [1, 2, 3, 4, 5, 9])) {
-                        $query->where('oms_order_creations.status_id', $request->status)->whereNotNull('oms_order_creations.assignee_user_id');
-                    } else {
-                        if(in_array($user->user_type_id, [6])) {
-                            $query->where('oms_order_creations.status_id', $request->status)->where('oms_order_creations.assignee_user_id', $user->id);
-
-                        } elseif(in_array($user->user_type_id, [7])) {
-                            $query->where('oms_order_creations.status_id', $request->status)->where('oms_order_creations.assignee_qa_id', $user->id);
-
-                        } elseif(in_array($user->user_type_id, [8])) {
-                            $query->where(function ($optionalquery) use($user) {
-                                $optionalquery->where('oms_order_creations.assignee_user_id', $user->id)
-                                    ->orWhere('oms_order_creations.assignee_qa_id', $user->id);
-                            });
-
-                        }
-                    }
-                } else {
-                    if(in_array($user->user_type_id, [1, 2, 3, 4, 5, 9])) {
-                        $query->where('oms_order_creations.status_id', $request->status)->whereNotNull('oms_order_creations.assignee_user_id');
-                    } elseif(in_array($user->user_type_id, [6])){
-                        $query->where('oms_order_creations.status_id', $request->status)->where('oms_order_creations.assignee_user_id', $user->id);
-                    }
-                    else if(in_array($user->user_type_id, [7])) {
-                        $query->where('oms_order_creations.status_id', $request->status)->Where('oms_order_creations.assignee_qa_id', $user->id);
-                    }else{
-                        $query->where(function ($optionalquery) use($user) {
-                            $optionalquery->where('oms_order_creations.assignee_user_id', $user->id)
-                                ->orWhere('oms_order_creations.assignee_qa_id', $user->id);
-                        });
-                    }
-                }
-            } elseif ($request->status == 'All') {
-                if(in_array($user->user_type_id, [6])) {
-                    $query->where('oms_order_creations.assignee_user_id', $user->id);
-                } elseif(in_array($user->user_type_id, [7])) {
-                    $query->where('oms_order_creations.assignee_qa_id', $user->id);
-                } elseif(in_array($user->user_type_id, [8])) {
-                    $query->where(function ($optionalquery) use($user) {
-                        $optionalquery->where('oms_order_creations.assignee_user_id', $user->id)
-                            ->orWhere('oms_order_creations.assignee_qa_id', $user->id);
-                    });
-                } else {
-                    $query->whereNotNull('oms_order_creations.assignee_user_id');
-                }
-            } elseif ($request->status == 6) {
-                if(in_array($user->user_type_id, [1, 2, 3, 4, 5, 6, 8, 9])) {
-                    $query->whereNull('oms_order_creations.assignee_user_id')->where('oms_order_creations.status_id', 1);
-                } else {
-                    $query->whereNull('oms_order_creations.id');
-                }
-            } elseif ($request->status == 7) {
-                if(in_array($user->user_type_id, [1, 2, 3, 4, 5, 7, 8, 9])) {
-                    $query->whereNull('oms_order_creations.assignee_qa_id')->where('oms_order_creations.status_id', 4);
-                } else {
-                    $query->whereNull('oms_order_creations.id');
-                }
-            }
-
-        $orderData = $query->first();
+        if (($user->user_type_id === 6 || $user->user_type_id === 8) && $orderData->status_id === 13 && empty($orderData->associate_id)) {
+            $orderData->update(['associate_id' => $user->id]);
+        } elseif (in_array($user->user_type_id, [1, 2, 3, 4, 5, 9]) && $orderData->status_id === 13) {
+            // No action needed, continue execution
+        } elseif (in_array($user->id, [$orderData->assignee_user_id, $orderData->assignee_qa_id, $orderData->associate_id])) {
+            // No action needed, continue execution
+        } else {
+            return redirect('/orders_status');
+        }
 
         return view('app.orders.coversheet_prep', compact('orderData'));
     }
