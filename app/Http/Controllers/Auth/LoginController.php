6657495adba53eee55bfd0038a3f6c7e104decfb
Shanmugam N <shanmugam@stellaripl.com>
2024-10-21 19:18:58 +0000

    Revert "Testing - Available Resources(3) Login controller"
    This reverts commit 23914517713edfa9943f936650dacf171ed3f2ed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,7 +38,7 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Request $request)
     {
 
         $this->middleware('guest')->except('logout');
