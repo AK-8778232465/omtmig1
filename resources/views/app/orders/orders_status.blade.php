3686f8fc4157c1ddcc8c94d5d158850684d31743
mani0807 <Manikandan.V@stellaripl.com>
2024-11-06 17:46:37 +0530

    2.Assign typist and typist_qc for Cypress, title, bc_law
    2.Assign typist and typist_qc for Cypress, title, bc_law

--- resources/views/app/orders/orders_status.blade.php
+++ resources/views/app/orders/orders_status.blade.php
@@ -1430,7 +1430,7 @@ $(document).on('change', 'input.check-one', function() {
 $(document).on('change', 'input.check-one', function() {
     var anyCheckboxChecked = $('input.check-one:checked').length > 0;
     if (anyCheckboxChecked) {
-        if ($(this).data('id') == 82 || $(this).data('id') == 86) {
+        if ($(this).data('id') == 82 || $(this).data('id') == 86 || $(this).data('id') == 84 || $(this).data('id') == 85) {
         $('#assign_tab').removeClass('d-none');
         $('#typist_qc_div').removeClass('d-none');
 
