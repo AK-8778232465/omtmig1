890c1542aecc12a828652cc13b29ef2b7ae53a6d
mani0807 <Manikandan.V@stellaripl.com>
2024-12-17 17:19:01 +0530

    Revert "Orderform unfreeze status dropdown"
    This reverts commit 086dbb848e8fc296f0bd7de67d51ee07643b68e1.

--- resources/views/app/orders/orderform.blade.php
+++ resources/views/app/orders/orderform.blade.php
@@ -1591,11 +1591,7 @@
                                             <div class="col-12 mb-2">
                                     <div class="font-weight-bold mb-1 mt-1">Status :</div>
                                         <input type="hidden" id="current_status_id" name="current_status_id" value="{{ $orderData->status_id }}">
-                                        @if(Auth::user()->hasRole('Typist') || Auth::user()->hasRole('Typist/Qcer') || Auth::user()->hasRole('Typist/Typist_Qcer'))
-                                            <select style=" "  class="form-control" name="order_status" id="order_status">
-                                        @else
                                         <select style=" "  class="form-control" name="order_status" id="order_status" @if(!isset($orderData->assignee_user)) disabled @endif>
-                                        @endif
                                             @if($orderData->status_id != 16)
                                             <option value="1" id="status_1" @if($orderData->status_id == 1) selected @endif>WIP</option>
                                                 <option value="4" id="status_4" @if($orderData->status_id == 4) selected @endif>Send for QC</option>
@@ -1681,11 +1677,7 @@
                                             <div class="col-10 mb-2">
                                                 <div class="font-weight-bold mb-1 mt-1">Status :</div>
                                                 <input type="hidden" id="current_status_id" name="current_status_id" value="{{ $orderData->status_id }}">
-                                                @if(Auth::user()->hasRole('Typist') || Auth::user()->hasRole('Typist/Qcer') || Auth::user()->hasRole('Typist/Typist_Qcer'))
-                                                <select style=" "  class="form-control" name="order_status" id="order_status">
-                                                @else
-                                                    <select style=" "  class="form-control" name="order_status" id="order_status" @if(!isset($orderData->assignee_user)) disabled @endif>
-                                                @endif
+                                                <select style="width:300px"  class="form-control" name="order_status" id="order_status" @if(!isset($orderData->assignee_user)) disabled @endif>
                                                     @if($orderData->status_id != 16)
                                                     <option value="1" id="status_1" @if($orderData->status_id == 1) selected @endif>WIP</option>
                                                         <option value="4" id="status_4" @if($orderData->status_id == 4) selected @endif>Send for QC</option>
@@ -1769,11 +1761,7 @@
                                             <div class="col-10 mb-2">
                                                 <div class="font-weight-bold mb-1 mt-1">Status :</div>
                                                 <input type="hidden" id="current_status_id" name="current_status_id" value="{{ $orderData->status_id }}">
-                                                @if(Auth::user()->hasRole('Typist') || Auth::user()->hasRole('Typist/Qcer') || Auth::user()->hasRole('Typist/Typist_Qcer'))
-                                                <select style=" "  class="form-control" name="order_status" id="order_status">
-                                                @else
-                                                    <select style=" "  class="form-control" name="order_status" id="order_status" @if(!isset($orderData->assignee_user)) disabled @endif>
-                                                @endif
+                                                    <select class="form-control" style="width:300px" name="order_status" id="order_status" @if(!isset($orderData->assignee_user)) disabled @endif>
                                                         @if(!Auth::user()->hasRole('Typist') && !Auth::user()->hasRole('Typist/Qcer') && !Auth::user()->hasRole('Typist/Typist_Qcer'))
                                                         @if($orderData->status_id != 16)
                                                         <option value="1" id="status_1" @if($orderData->status_id == 1) selected @endif>WIP</option>
@@ -1858,11 +1846,7 @@
                                             <div class="col-10 mb-2">
                                                 <div class="font-weight-bold mb-1 mt-1">Status :</div>
                                                     <input type="hidden" id="current_status_id" name="current_status_id" value="{{ $orderData->status_id }}">
-                                                    @if(Auth::user()->hasRole('Typist') || Auth::user()->hasRole('Typist/Qcer') || Auth::user()->hasRole('Typist/Typist_Qcer'))
-                                                    <select style=" "  class="form-control" name="order_status" id="order_status">
-                                                @else
-                                                    <select style=" "  class="form-control" name="order_status" id="order_status" @if(!isset($orderData->assignee_user)) disabled @endif>
-                                                @endif
+                                                    <select class="form-control" style="width:300px" name="order_status" id="order_status" @if(!isset($orderData->assignee_user)) disabled @endif>
                                                         @if(!Auth::user()->hasRole('Typist') && !Auth::user()->hasRole('Typist/Qcer'))
                                                         <option value="1" id="status_1" @if($orderData->status_id == 1) selected @endif>WIP</option>
                                                         <!-- @if(in_array($orderData->stl_process_id, [12, 7, 8, 9]) || !in_array($orderData->client_id, [84, 85, 86]))
